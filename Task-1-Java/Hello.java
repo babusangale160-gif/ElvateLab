ce76502b887d74edda4f4745d4be38be182a7eb0
babusangale160-gif <babusangale160@gmail.com>
2026-01-15 20:43:39 +0530

    Add files via upload

---
+++ Task-1-Java/Hello.java
@@ -0,0 +1,9 @@
+// hello this is my first java program
+ public class Hello
+{
+
+public static void main(String [] args)
+   {
+	System.out.println("Hello, Elevate Labs!***My Self Babu Sangale***");
+   }
+}
